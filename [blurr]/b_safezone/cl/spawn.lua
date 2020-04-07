015f49b1bcb20aa874c565e4fd91dafade710080
BoundlessRP <63270322+BoundlessRP@users.noreply.github.com>
2020-04-06 22:02:26 -0700

    Mostly finished client side of trader spawning

--- [blurr]/b_safezone/cl/spawn.lua
+++ [blurr]/b_safezone/cl/spawn.lua
@@ -5,7 +5,9 @@ local traderSpawns = {
 }
 local traders = {}
 
-function SpawnTraders()
+RegisterNetEvent('spawn:spawnTraders')
+AddEventHandler('spawn:spawnTraders', function()
+	traders = {}
 	for i,v in pairs(traders) do
 		RequestModel(GetHashKey(v.model))
 		while (HasModelLoaded(GetHashKey(v.model)) == false) do
@@ -16,5 +18,32 @@ function SpawnTraders()
 		local ped = CreatePed(4, GetHashKey(v.model), v.coords.x, v.coords.y, v.coords.z, 0, false, true)
 		SetEntityHeading(ped, v.coords.h)
 		SetEntityInvincible(ped, true)
+		table.insert(traders, {t=v.t, ped=ped})
+		TriggerServerEvent('spawn:updateTraders', traders)
 	end	
 end
+
+RegisterNetEvent('spawn:updateTraders')
+AddEventHandler('spawn:updateTraders', function(t)
+	traders = t
+end)
+
+Citizen.CreateThread(function()
+	while (#traders == 0) do
+		Citizen.Wait(100)
+	end
+	
+	while true do
+		Citizen.Wait(1)
+
+		for i,v in pairs(traders) do
+			local plyCoords = GetEntityCoords(GetPlayerPed(-1))
+			local traCoords = GetEntityCoords(v.ped)
+			local distance = GetDistanceBetweenCoords(plyCoords.x, plyCoords.y, plyCoords.z, traCoords.x, traCoords.y, traCoords.z, true)
+
+			if (distance <= 3.0) then
+				-- We are next to a trader..
+			end
+		end
+	end
+end)
