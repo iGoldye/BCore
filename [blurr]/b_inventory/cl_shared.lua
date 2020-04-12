ce68150ee04bf7bbf9dc773f4e8fa1279356f707
BoundlessRP <63270322+BoundlessRP@users.noreply.github.com>
2020-04-12 06:23:41 -0700

    Client side of trading

--- [blurr]/b_inventory/cl_shared.lua
+++ [blurr]/b_inventory/cl_shared.lua
@@ -1,5 +1,6 @@
 inv = {}
-otherInv = {}
+invWeight = 0
+invCount = 0
 
 inv_ui = false
 craft_ui = false
@@ -7,10 +8,12 @@ trade_ui = false
 vehicle_ui = false
 
 canOpenCrafting = false
-
 crafting_locations = {
 	{name="workbench", x=0.0, y=0.0, z=0.0},
 }
+
+tradeName = "Other Players Name"
+otherInv = {}
 
 function DrawText3D(x, y, z, r, g, b, text)
     local onScreen,_x,_y=World3dToScreen2d(x,y,z)
--- [blurr]/b_inventory/cl_trading.lua
+++ [blurr]/b_inventory/cl_trading.lua
@@ -1,5 +1,42 @@
+RegisterNUICallback('requestTrading', function()
+	SetNuiFocus(false, false)
+	inv_ui = false
+
+	otherInv = {}
+	tradeName = "Other Players Name"
+
+	local t, distance = GetClosestPlayer()
+	if(distance ~= -1 and distance < 3) then
+		TriggerServerEvent("trade:requestTrade", GetPlayerServerId(t))
+	end
+end)
+
+RegisterNetEvent('trade:fail')
+AddEventHandler('trade:fail', function()
+	print("Oopsie, I made a poopsie.")
+end)
+
+RegisterNetEvent('trade:open')
+AddEventHandler('trade:open', function(inventory, weight, count, otherInventory, name)
+	inv = inventory
+	invWeight = weight
+	invCount = count
+
+	otherInv = otherInventory
+	tradeName = name
+
+	SendNUIMessage({
+    	action = 'open_inventory',
+    	items = inv
+  	})
+
+	trade_ui = true
+  	SetNuiFocus(true, true)
+end)
+
 RegisterNUICallback('closeTrading', function()
 	SetNuiFocus(false, false)
 	trade_ui = false
 	otherInv = {}
+	tradeName = "Other Players Name"
 end)
