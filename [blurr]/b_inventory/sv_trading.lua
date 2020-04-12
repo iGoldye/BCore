3f9607927044051c69fd8068d602105f954f9463
BoundlessRP <63270322+BoundlessRP@users.noreply.github.com>
2020-04-12 06:36:23 -0700

    Server side request trade

---
+++ [blurr]/b_inventory/sv_trading.lua
@@ -0,0 +1,27 @@
+RegisterServerEvent('trade:requestTrade')
+AddEventHandler('trade:requestTrade', function(target)
+	TriggerEvent('b:getPlayer', source, function(user)
+		if (user ~= nil) then
+			local inventory = user.GetItemInventory()
+			local weight = user.GetItemsWeight()
+			local count = user.GetItemsCount()
+			local name = user.GetCharacterName()
+			
+			TriggerEvent('b:getPlayer', target, function(otherUser)
+				if (otherUser ~= nil) then
+					local otherInventory = otherUser.GetItemInventory()
+					local otherWeight = otherUser.GetItemsWeight()
+					local otherCount = otherUser.GetItemsCount()
+					local otherName = otherUser.GetCharacterName()
+
+					TriggerClientEvent('trade:open', source, inventory, weight, count, otherInventory, otherName)
+					TriggerClientEvent('trade:open', target, otherInventory, otherWeight, otherCount, inventory, name)
+				else
+					TriggerClientEvent('trade:fail', source)
+				end
+			end)
+		else
+			TriggerClientEvent('trade:fail', source)
+		end
+	end)
+end)
